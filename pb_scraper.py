e007a4b0f1e2129418e9e280169fda85e30342b2
Joseph Webster <rwc.webster@gmail.com>
2024-05-24 10:32:52 +0100

    updated to more specific methods

--- pb_scraper.py
+++ pb_scraper.py
@@ -10,45 +10,150 @@ class TorrentFinder:
         pass
 
     # method to search for torrents on pirate bay
-    def search_pirate_bay(self, query):
-        url = 'https://piratebay.live/search/' + query + '/1/99/0'
+    def search_hd_movies(self, query):
+        url = 'https://prbay.top/search/' + query + '/1/99/207'
         html = requests.get(url).text
         soup = BeautifulSoup(html, 'html.parser')
 
         results = []
-        x = 0
         for trs in soup.find_all('tr'):
             tds = trs.find_all('td')
-            # use re to get title from element with detLink class
-            title = re.search(r'(?<=title=")(.*)(?=">)', str(tds[1])).group(0) if len(tds) > 1 else None
 
-            if title is None:
-                continue
+            if len(tds) > 1:
+                # Find the link tag containing the magnet link
+                magnet_link_tag = tds[1].find('a', href=True, title="Download this torrent using magnet")
 
-            # if Details for sting is in title, remove it
-            if 'Details for ' in title:
-                title = title.replace('Details for ', '')
+                if magnet_link_tag:
+                    magnet = magnet_link_tag['href']
+                else:
+                    magnet = None
 
-            # get magnet link
-            magnet = re.search(r'(?<=href=")(.*)(?=" title="Download this torrent using magnet")', str(tds[1])).group(0) if len(tds) > 1 else None
+                title_tag = tds[1].find('a', class_='detLink')
+                if title_tag:
+                    title = title_tag.get('title', '').replace('Details for ', '')
+                else:
+                    title = None
 
-            # get size
-            size = re.search(r'(?<=Size )(.*)(?=,)', str(tds[1])).group(0) if len(tds) > 1 else None
+                size_match = re.search(r'(?<=Size )(.*)(?=,)', str(tds[1]))
+                size = size_match.group(0) if size_match else None
 
-            # get seeders
-            seeders = re.search(r'(?<=<td align="right">)(.*)(?=</td>)', str(tds[2])).group(0) if len(tds) > 2 else None
+                seeders = tds[2].text if len(tds) > 2 else None
+                leechers = tds[3].text if len(tds) > 3 else None
 
-            # get leechers
-            leechers = re.search(r'(?<=<td align="right">)(.*)(?=</td>)', str(tds[3])).group(0) if len(tds) > 3 else None
-
-            dict = { 'title': title, 'magnet': magnet, 'size': size, 'seeders': seeders, 'leechers': leechers }
-            # replace any  \xa0 with a space
-            dict = { key: value.replace('\xa0', ' ') for key, value in dict.items() }
-
-            results.append(dict)
+                result = { 'title': title, 'magnet': magnet, 'size': size, 'seeders': seeders, 'leechers': leechers }
+                result = { key: value.replace('\xa0', ' ') if value else value for key, value in result.items() }
+                results.append(result)
 
         return results
 
+    def search_movies(self, query):
+        url = 'https://prbay.top/search/' + query + '/1/99/201'
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        results = []
+        for trs in soup.find_all('tr'):
+            tds = trs.find_all('td')
+
+            if len(tds) > 1:
+                # Find the link tag containing the magnet link
+                magnet_link_tag = tds[1].find('a', href=True, title="Download this torrent using magnet")
+
+                if magnet_link_tag:
+                    magnet = magnet_link_tag['href']
+                else:
+                    magnet = None
+
+                title_tag = tds[1].find('a', class_='detLink')
+                if title_tag:
+                    title = title_tag.get('title', '').replace('Details for ', '')
+                else:
+                    title = None
+
+                size_match = re.search(r'(?<=Size )(.*)(?=,)', str(tds[1]))
+                size = size_match.group(0) if size_match else None
+
+                seeders = tds[2].text if len(tds) > 2 else None
+                leechers = tds[3].text if len(tds) > 3 else None
+
+                result = { 'title': title, 'magnet': magnet, 'size': size, 'seeders': seeders, 'leechers': leechers }
+                result = { key: value.replace('\xa0', ' ') if value else value for key, value in result.items() }
+                results.append(result)
+
+        return results
+
+    def search_hd_tv_shows(self, query):
+        url = 'https://prbay.top/search/' + query + '/1/99/208'
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        results = []
+        for trs in soup.find_all('tr'):
+            tds = trs.find_all('td')
+
+            if len(tds) > 1:
+                # Find the link tag containing the magnet link
+                magnet_link_tag = tds[1].find('a', href=True, title="Download this torrent using magnet")
+
+                if magnet_link_tag:
+                    magnet = magnet_link_tag['href']
+                else:
+                    magnet = None
+
+                title_tag = tds[1].find('a', class_='detLink')
+                if title_tag:
+                    title = title_tag.get('title', '').replace('Details for ', '')
+                else:
+                    title = None
+
+                size_match = re.search(r'(?<=Size )(.*)(?=,)', str(tds[1]))
+                size = size_match.group(0) if size_match else None
+
+                seeders = tds[2].text if len(tds) > 2 else None
+                leechers = tds[3].text if len(tds) > 3 else None
+
+                result = { 'title': title, 'magnet': magnet, 'size': size, 'seeders': seeders, 'leechers': leechers }
+                result = { key: value.replace('\xa0', ' ') if value else value for key, value in result.items() }
+                results.append(result)
+
+        return results
+
+    def search_tv_shows(self, query):
+        url = 'https://prbay.top/search/' + query + '/1/99/205'
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        results = []
+        for trs in soup.find_all('tr'):
+            tds = trs.find_all('td')
+
+            if len(tds) > 1:
+                # Find the link tag containing the magnet link
+                magnet_link_tag = tds[1].find('a', href=True, title="Download this torrent using magnet")
+
+                if magnet_link_tag:
+                    magnet = magnet_link_tag['href']
+                else:
+                    magnet = None
+
+                title_tag = tds[1].find('a', class_='detLink')
+                if title_tag:
+                    title = title_tag.get('title', '').replace('Details for ', '')
+                else:
+                    title = None
+
+                size_match = re.search(r'(?<=Size )(.*)(?=,)', str(tds[1]))
+                size = size_match.group(0) if size_match else None
+
+                seeders = tds[2].text if len(tds) > 2 else None
+                leechers = tds[3].text if len(tds) > 3 else None
+
+                result = { 'title': title, 'magnet': magnet, 'size': size, 'seeders': seeders, 'leechers': leechers }
+                result = { key: value.replace('\xa0', ' ') if value else value for key, value in result.items() }
+                results.append(result)
+
+        return results
+        
 
 if __name__ == '__main__':
     pass
